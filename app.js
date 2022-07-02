10720155679ed63c5acb362c874b92ba213317c7
Neha Valeti <nehavaleti01@gmail.com>
2022-07-02 10:30:08 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const date = require(__dirname + "/date.js");
+
+//console.log(date());
+const app = express();
+const items = ["buy food", "eat food"];
+const workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+  const day = date.getDate();
+  res.render("list", {
+    listTitle: day,
+    newListItems: items,
+  });
+});
+
+app.post("/", function (req, res) {
+  const item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+  //console.log(input);
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems,
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+// app.post("/work", function (req, res) {
+//   let item = req.body.newItem;
+//   workItems.push(item);
+//   res.redirect("/work");
+// });
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
